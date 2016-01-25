97b1d858eb3a0d80df13f651b4feb5c0ca045b85
Chrisp <ultrachrisp@gmail.com>
2016-01-25 22:05:53 +0200

    Favicons added and holding page is up.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/*global require*/
+
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
@@ -7,8 +9,8 @@ var gulp = require('gulp'),
 	rename = require('gulp-rename'),
 	concat = require('gulp-concat'),
 	notify = require('gulp-notify'),
-	cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
+	//cache = require('gulp-cache'),
+  livereload = require('gulp-livereload'),
 	nodeReset = require('node-reset-scss'),
 	order = require('gulp-order'),
 	del = require('del');
@@ -17,7 +19,7 @@ gulp.task('styles', function () {
 	return gulp.src('src/scss/main.scss')
 		.pipe(sass({includePaths: [nodeReset.includePath, './node_modules/susy/sass', './bourbon']}))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	    .pipe(gulp.dest('static/css'))
+	  .pipe(gulp.dest('static/css'))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minifycss())
 		.pipe(gulp.dest('static/css'))
@@ -25,10 +27,10 @@ gulp.task('styles', function () {
 });
 
 gulp.task('scripts', function () {
-	return gulp.src('src/js/**/*.js')
-        .pipe(order([
-            "myScripts.js"
-        ]))
+	return gulp.src('src/js/*.js')
+    .pipe(order([
+        "myScripts.js"
+    ]))
 		.pipe(concat('main.js'))
 		.pipe(gulp.dest('static/js'))
 		.pipe(rename({suffix: '.min'}))
@@ -44,16 +46,22 @@ gulp.task('images', function () {
 		.pipe(notify({ message: 'Images task completed' }));
 });
 
-gulp.task('clean', function (cb) {
-	del(['static/css', 'static/js', 'static/img', 'public/**', '!public'], cb);
+gulp.task('general', function () {
+  return gulp.src('src/general/**')
+    .pipe(gulp.dest('static/'));
 });
 
-gulp.task('default', ['clean', 'styles', 'scripts', 'images']);
+gulp.task('clean', function (cb) {
+	del(['static/**', '!static', 'public/**', '!public'], cb);
+});
+
+gulp.task('default', ['clean', 'styles', 'scripts', 'images', 'general']);
 
 gulp.task('watch', function () {
 	livereload.listen();
 	gulp.watch('src/scss/**/*.scss', ['styles']);
 	gulp.watch('src/js/**/*.js', ['scripts']);
 	gulp.watch('src/img/**/*', ['images']);
+  gulp.watch('src/general/**/*', ['general']);
 	gulp.watch(['static/**']).on('change', livereload.changed);
 });
