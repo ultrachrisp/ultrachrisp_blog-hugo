10ca1b94643185d7b8d55500869e5633a58f352f
Chris Pyke <chris.pyke@absa.co.za>
2017-03-05 09:04:10 +0200

    Trying org posts.

--- gulpfile.js
+++ gulpfile.js
@@ -11,52 +11,52 @@ var gulp = require('gulp'),
 	notify = require('gulp-notify'),
     livereload = require('gulp-livereload'),
 	nodeReset = require('node-reset-scss'),
-    fontAwesome = require('node-font-awesome');
-	order = require('gulp-order'),
-	del = require('del');
+    fontAwesome = require('node-font-awesome'),
+    order = require('gulp-order'),
+    del = require('del');
 
 gulp.task('clean', function (cb) {
 	del(['static/**', '!static', 'public/**', '!public'], cb);
 });
 
 gulp.task('general', function () {
-  return gulp.src('src/general/**')
-    .pipe(gulp.dest('static'));
+    return gulp.src('src/general/**')
+           .pipe(gulp.dest('static'));
 });
 
 gulp.task('styles', function () {
 	return gulp.src('src/scss/main.scss')
-		.pipe(sass({includePaths: [nodeReset.includePath, './node_modules/susy/sass', fontAwesome.scssPath, './bourbon']}))
-		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	  .pipe(gulp.dest('static/css'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(cssnano())
-		.pipe(gulp.dest('static/css'))
-		.pipe(notify({message: 'Styles task complete' }));
+		   .pipe(sass({includePaths: [nodeReset.includePath, './node_modules/susy/sass', fontAwesome.scssPath, './bourbon']}))
+		   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+	       .pipe(gulp.dest('static/css'))
+		   .pipe(rename({suffix: '.min'}))
+		   .pipe(cssnano())
+		   .pipe(gulp.dest('static/css'))
+		   .pipe(notify({message: 'Styles task complete' }));
 });
 
 gulp.task('scripts', function () {
 	return gulp.src('src/js/*.js')
-    .pipe(order([
-        "myScripts.js"
-    ]))
-		.pipe(concat('main.js'))
-		.pipe(gulp.dest('static/js'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(uglify())
-		.pipe(gulp.dest('static/js'))
-		.pipe(notify({ message: 'Scripts task complete' }));
+           .pipe(order([
+               "myScripts.js"
+           ]))
+		   .pipe(concat('main.js'))
+		   .pipe(gulp.dest('static/js'))
+		   .pipe(rename({suffix: '.min'}))
+		   .pipe(uglify())
+		   .pipe(gulp.dest('static/js'))
+		   .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 gulp.task('images', function () {
 	return gulp.src('src/img/**/*')
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('static/img'))
-		.pipe(notify({ message: 'Images task completed' }));
+		   .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+		   .pipe(gulp.dest('static/img'))
+		   .pipe(notify({ message: 'Images task completed' }));
 });
 
 gulp.task('fonts', function() {
-  gulp.src(fontAwesome.fonts)
+    gulp.src(fontAwesome.fonts)
     .pipe(gulp.dest('static/fonts'));
 });
 
@@ -64,7 +64,7 @@ gulp.task('default', ['clean', 'general', 'styles', 'scripts', 'images', 'fonts'
 
 gulp.task('watch', function () {
 	livereload.listen();
-  gulp.watch('src/general/**/*', ['general']);
+    gulp.watch('src/general/**/*', ['general']);
 	gulp.watch('src/scss/**/*.scss', ['styles']);
 	gulp.watch('src/js/**/*.js', ['scripts']);
 	gulp.watch('src/img/**/*', ['images']);
