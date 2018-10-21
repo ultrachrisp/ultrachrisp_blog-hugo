00b7aa54ea18d27f139a57c669c5be7202b69be0
Chrisp <ultrachrisp@gmail.com>
2018-10-21 11:26:20 +0200

    Still tweaking submodules.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Keeping this in reserve for now
+# Ensure submodule is in master
+# cd public
+# git checkout master
+# cd ..
+
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
