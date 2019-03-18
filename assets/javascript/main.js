d07a1bccc006c96d511ab5e7ab359ff893508042
Christopher Pyke <chrisp@Christophers-MacBook-Pro-3.local>
2019-03-18 15:34:42 +0200

    Working on css grid, also finding local optima for tiles

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -27,12 +27,34 @@ const createObject = () => {
 const setCanvasSize = ({anim}) => {
     const availableWidth = anim.canvas.width = anim.canvas.height = anim.element.getBoundingClientRect().width;
 
-    // console.log(availableWidth,' ',availableWidth / anim.svgWidth,' ',availableWidth % anim.svgWidth);
-    if(availableWidth % anim.svgWidth > 1){
-        let temp1 = (availableWidth) % (anim.svgWidth + 1);
-        let temp2 = (availableWidth) % (anim.svgWidth - 1);
-        console.log(temp1,' ',temp2);
-    }
+    // if(availableWidth % anim.svgWidth > 1){
+    const space = availableWidth % anim.svgWidth,
+          halfTile = anim.svgWidth / 2,
+          temp3 = (space > halfTile)?
+          tileSizeIncrease({
+              width: availableWidth,
+              limit: anim.svgWidth,
+              num: anim.svgWidth+1 }):
+          tileSizeDecrease({
+              width: availableWidth,
+              limit: anim.svgWidth,
+              num: anim.svgWidth-1}) ;
+    console.log(space,' ',halfTile,' ',temp3,' ',availableWidth,' ',anim.svgWidth,' ',availableWidth % anim.svgWidth,' ',availableWidth / temp3);
+    anim.svgWidth = temp3;
+
+    console.log(anim.svgWidth);
+        return ;
+    // }
+};
+
+const tileSizeDecrease = ({width, num}) => {
+    // ensure we are still reducing
+    return (width % num > num)? tileSizeDecrease({width, num: num-1}) : num;
+};
+
+const tileSizeIncrease = ({width, limit, num}) => {
+    // local optima
+    return (width % num < limit)? tileSizeIncrease({width, limit, num: num+1}) : num-1;
 };
 
 initDashboard();
